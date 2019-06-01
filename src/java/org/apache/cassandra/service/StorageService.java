218cd61de3b9a6f15183eb661827c5fdb102d2fc
Stevie_Wu <wuit@bc.edu>
2019-05-31 22:46:47 -0400

    Add logger to see what library path I have

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -711,6 +711,16 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
         initialized = true;
         logger.debug("Initiate our Erasure Coding");
+        String javaLibPath = System.getProperty("java.library.path");
+        Map<String, String> envVars = System.getenv();
+        logger.debug(envVars.get("Path"));
+        logger.debug(javaLibPath);
+        for (String var : envVars.keySet()) {
+            logger.debug("examining " + var);
+            if (envVars.get(var).equals(javaLibPath)) {
+                logger.debug(var);
+            }
+        }
         StorageProxy.initiateErasureCode();
         TreasConfig.initiateAddressMap();
     }
