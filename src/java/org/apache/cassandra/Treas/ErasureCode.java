b73d8595b409d02afb04b62ae9dfeeeb3a1625c8
Stevie_Wu <wuit@bc.edu>
2019-06-01 11:51:16 -0400

    Load library

--- src/java/org/apache/cassandra/Treas/ErasureCode.java
+++ src/java/org/apache/cassandra/Treas/ErasureCode.java
@@ -30,6 +30,7 @@ public class ErasureCode
     {
         try
         {
+            System.loadLibrary("isal");
             System.loadLibrary("erasure");
         }
         catch ( Exception e )
