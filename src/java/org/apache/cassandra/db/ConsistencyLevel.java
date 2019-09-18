33694009f8603807f0e43616eecb312edee7b1b2
Stevie_Wu <wuit@bc.edu>
2019-09-17 20:29:39 -0400

    Seperate out writeback logic from normal write

--- src/java/org/apache/cassandra/db/ConsistencyLevel.java
+++ src/java/org/apache/cassandra/db/ConsistencyLevel.java
@@ -275,7 +275,7 @@ public enum ConsistencyLevel
                 break;
             case TREAS:
                 int currentLive = countLocalEndpoints(liveEndpoints);
-                logger.debug( "CurrentLive: " + currentLive + " , Needed: " + TreasConfig.QUORUM);
+                //logger.debug( "CurrentLive: " + currentLive + " , Needed: " + TreasConfig.QUORUM);
                 if (currentLive < TreasConfig.QUORUM) {
                     throw new UnavailableException(this, TreasConfig.QUORUM, currentLive);
                 }
