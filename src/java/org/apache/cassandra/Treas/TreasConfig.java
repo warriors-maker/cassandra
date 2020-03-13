38a824a9fce23a24903438490c461c10b62c2b10
Steven <wuit@bc.edu>
2020-03-13 01:52:56 -0400

    Config

--- src/java/org/apache/cassandra/Treas/TreasConfig.java
+++ src/java/org/apache/cassandra/Treas/TreasConfig.java
@@ -25,7 +25,7 @@ import org.apache.cassandra.cql3.ColumnIdentifier;
 
 public class TreasConfig
 {
-    public final static int num_server = 5;
+    public final static int num_server = 11;
     public final static int num_intersect = 5;
     public final static int num_recover = 2;
     public final static int num_concurrecy = 3;
